1ba46d70a67349126126adee78a7e89ece4083b6
RAVI TEJA BELLAM <braviteja773@gmail.com>
2024-11-01 15:33:33 -0400

    Update DbUtils.java

--- Elearning/src/config/DbUtils.java
+++ Elearning/src/config/DbUtils.java
@@ -7,7 +7,7 @@ public class DbUtils {
 	public static Connection connect() throws Exception {
 		//Class.forName("com.mysql.jdbc.Driver");
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		final String URL="jdbc:C://Program Files/MySQLMySQL Server 8.0/my.ini/DESKTOP-4MF2OSG/elearning";
+		final String URL="jdbc:mysql://localhost/elearning/";
 		Connection con=DriverManager.getConnection(URL, "root", "50014");
 		return con;
 	}
